bdc9b8282dcc7cb7ad03a7809255a2466e0a89ef
JoaoPstein <vulgostein@gmail.com>
2023-07-18 20:48:11 -0300

    init

---
+++ src/main/java/model/JavaBeans.java
@@ -0,0 +1,63 @@
+package model;
+
+public class JavaBeans {
+	
+	private String id;
+	private String name;
+	private String registration;
+	private String address;
+	private String enrollmentModality;
+	private String personType;
+	
+	public JavaBeans() {
+		super();
+	}
+	
+	public JavaBeans(String name, String registration, String address, String enrollmentModality, String personType) {
+		super();
+		
+		this.name = name;
+		this.registration = registration;
+		this.address = address;
+		this.enrollmentModality = enrollmentModality;
+		this.personType = personType;
+	}
+	
+	public String getId(){
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public void setName(String nome) {
+		this.name = nome;
+	}
+	public String getRegistration() {
+		return registration;
+	}
+	public void setRegistrattion(String matricula) {
+		this.registration = matricula;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String endereco) {
+		this.address = endereco;
+	}
+	public String getEnrollmentModality() {
+		return enrollmentModality;
+	}
+	public void setEnrollmentModality(String modalidadeMatricula) {
+		this.enrollmentModality = modalidadeMatricula;
+	}
+	public String getPersonType() {
+		return personType;
+	}
+	public void setPersonType(String tipoPessoa) {
+		this.personType = tipoPessoa;
+	}
+}
---
+++ src/main/webapp/client.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ page import="java.util.ArrayList"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Clientes</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Clientes</h1><a href="novo.html" class="Botao1">Novo cliente</a>
+	<table id="tabela">
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Nome</th>
+				<th>Tipo pessoa</th>
+				<th>Endereço</th>
+				<th>Matricula</th>
+				<th>Tipo da matricula</th>
+			</tr>
+		</thead>
+		
+	</table>
+	<script src="scripts/confirmador.js"></script>
+</body>
+</html>
